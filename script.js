23d26f75de3678938e4cc0b408d36aea14de9633
Al Mahmud <mdalmahmud0183@gmail.com>
2018-05-21 03:04:19 +0000

    task2

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 /* JavaScript code here */
-console.log("external js")
+//task 1
+console.log("external js");
+//task 2
+function sumArray (arr){
+    var sum =0;
+    arr.forEach(function(value, index){
+        sum += value;
+    });
+    return sum;
+}
+console.log(sumArray([1,2,3,4,5]));
+console.log(sumArray([19,-2,30,-45]));
